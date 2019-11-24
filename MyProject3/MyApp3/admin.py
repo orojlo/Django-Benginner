a2db03830072a32d010ade8c7dd533e5d34796a9
AmirHosein Orojlo <a.orojlo@gmail.com>
2019-11-24 23:18:47 +0330

    Add Project3 Django Admin SQLITE And models.Model

---
+++ MyProject3/MyApp3/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from MyApp3.models import AccressRecord,Topic,Webpage
+# Register your models here.
+admin.site.register(AccressRecord)
+admin.site.register(Topic)
+admin.site.register(Webpage)
