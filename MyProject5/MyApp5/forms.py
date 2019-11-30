83329d674bd58dce1c2cf70d7d3c4cf02885797d
orojlo <a.orojlo@gmail.com>
2019-11-30 17:47:27 +0330

    Add Form Validations

--- MyProject5/MyApp5/forms.py
+++ MyProject5/MyApp5/forms.py
@@ -1,6 +1,31 @@
 from django import forms
+from django.core import validators
+
+# def check_for_z(value):
+#     if value[0].lower() != 'z':
+#         raise forms.ValidationError("Needs To Start With Z")
+
 
 class FormName(forms.Form):
-    name = forms.CharField()
+    name = forms.CharField() # validators=[check_for_z]
     email = forms.EmailField()
+    verify_email = forms.EmailField(label='Enter Your Email Again:')
     text = forms.CharField(widget=forms.Textarea)
+
+
+    def clean(self):
+        all_clean_data = super().clean()
+        email = all_clean_data['email']
+        vmail = all_clean_data['verify_email']
+
+        if email != vmail:
+            raise forms.ValidationError("Are You Sure?")
+    # botcatcher = forms.CharField(required=False,
+    #                             widget=forms.HiddenInput,
+    #                             validators=[validators.MaxLengthValidator(0)])
+
+# def clean_botcatcher(self):
+#     botcatcher = self.cleaned_data['botcatcher']
+#     if len(botcatcher) > 0:
+#         raise forms.ValidationError("GOTCHA BOT!")
+#     return botcatcher
