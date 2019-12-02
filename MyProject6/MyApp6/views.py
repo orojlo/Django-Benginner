3936edbffbf652c36f52d8282153ea46ec5db864
orojlo <a.orojlo@gmail.com>
2019-12-02 19:50:50 +0330

    Add Filter Coding

--- MyProject6/MyApp6/views.py
+++ MyProject6/MyApp6/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'MyApp6/index.html')
+    context_dict = {'text':'Hello AmirHosein','number':100}
+    return render(request,'MyApp6/index.html',context_dict)
 
 def other(request):
     return render(request,'MyApp6/other.html')
