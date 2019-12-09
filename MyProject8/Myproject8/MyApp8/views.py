9c6865632aa45dde9e0ffdee1618153442cbfcf0
orojlo <a.orojlo@gmail.com>
2019-12-09 17:02:31 +0330

    DetailView And ListView Part 1

--- MyProject8/Myproject8/MyApp8/views.py
+++ MyProject8/Myproject8/MyApp8/views.py
@@ -1,14 +1,36 @@
 from django.shortcuts import render
-from django.views.generic import  View,TemplateView
+from django.views.generic import  View,TemplateView,ListView,DetailView
+from . import models
 #from django.http import HttpResponse
 
-class IndexView(TemplateView):
-    template_name = 'index.html'
 
-    def get_context_data(self,**kwargs):
-        context = super().get_context_data(**kwargs)
-        context['injectme'] = 'Basic Injection!'
-        return context
+class SchoolListView(ListView):
+    model = models.School
+
+class SchoolDetailView(DetailView):
+    model = models.School
+    template_name = 'MyApp/school_detail.html'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class IndexView(TemplateView):
+#     template_name = 'index.html'
+#
+#     def get_context_data(self,**kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['injectme'] = 'Basic Injection!'
+#         return context
 
 
 # class CBView(View):
