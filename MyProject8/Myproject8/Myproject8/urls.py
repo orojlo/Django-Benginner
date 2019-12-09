d6e0d399dc2a185403a02b89599205b884e6d518
orojlo <a.orojlo@gmail.com>
2019-12-09 16:24:55 +0330

    Template View In CBV

--- MyProject8/Myproject8/Myproject8/urls.py
+++ MyProject8/Myproject8/Myproject8/urls.py
@@ -19,5 +19,5 @@ from MyApp8 import views
 from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$',views.CBView.as_view()),
+    url(r'^$',views.IndexView.as_view()),
 ]
